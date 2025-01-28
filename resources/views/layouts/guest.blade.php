45977b88224f77f449d76e2723b4ae0a31f0b6b0
rayague <rayague03@gmail.com>
2025-01-28 11:10:56 +0100

    correction

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,20 +11,23 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="relative font-sans antialiased text-gray-900">
+<body class="relative flex flex-col items-center justify-center min-h-screen font-sans antialiased text-gray-900">
     <img src="{{ asset('/images/image4.jpg') }}" class="fixed top-0 bottom-0 left-0 w-full h-screen -z-10"
         alt="">
-    <div class="flex flex-col items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0">
+    {{-- <div class="flex flex-col items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0"> --}}
 
-        <div class="absolute z-10 w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md sm:rounded-lg">
-            {{ $slot }}
-        </div>
+    <div
+        class="absolute z-10 w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-lg shadow-slate-950 sm:max-w-md sm:rounded-lg">
+        {{ $slot }}
     </div>
+    {{-- </div> --}}
 </body>
 
 </html>
