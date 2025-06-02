4660b414a2ddedeb6f85aaa396e68a21a36183f4
Ray Ague <111430449+rayague@users.noreply.github.com>
2025-06-02 16:12:01 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,19 +78,17 @@
                         Tableau de bord
                     </a>
                 @else
-                    <a href="{{ route('login') }}"
-                        class="rounded-md bg-yellow-500 p-2 text-black
-                        ring-1 ring-transparent transition hover:text-black/70 focus:outline-none
-                        focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80
-                        dark:focus-visible:ring-white">
-                        Connexion
-                    </a>
-                    {{-- <button id="login-btn" type="button"
-                        class="rounded-md bg-yellow-500  p-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-
-
-                        Connexion
-                    </button> --}}
+{{-- <a href="{{ route('login') }}"
+    class="rounded-md bg-yellow-500 p-2 text-black
+    ring-1 ring-transparent transition hover:text-black/70 focus:outline-none
+    focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80
+    dark:focus-visible:ring-white">
+    Connexion
+</a> --}}
+<button id="login-btn" type="button"
+    class="rounded-md bg-yellow-500  p-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hove[...">
+    Connexion
+</button>
 
                     {{-- @if (Route::has('register'))
                         <a href="{{ route('register') }}"
